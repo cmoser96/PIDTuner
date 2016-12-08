c16febb68e7aab328cd7c63bf65db84b8ecc0064
Serena Chen <serenazychen@gmail.com>
2016-12-08 12:08:41 -0500

    added overloaded constructor for pid tuner, changed ranges

--- src/PIDTuner.java
+++ src/PIDTuner.java
@@ -17,9 +17,9 @@ public class PIDTuner {
             score = new double[NUM_TESTS];
     private Vector3D originalPos;
 
-    public static final double D_UPPER = 100.0, D_LOWER = -100.0, D_RANGE = D_UPPER-D_LOWER;
-    public static final double P_UPPER = 100.0, P_LOWER = -100.0, P_RANGE = P_UPPER-P_LOWER;
-    public static final double I_UPPER = 100.0, I_LOWER = -100.0, I_RANGE = I_UPPER-I_LOWER;
+    public static final double D_UPPER = 100.0, D_LOWER = 0.0, D_RANGE = D_UPPER-D_LOWER;
+    public static final double P_UPPER = 100.0, P_LOWER = 0.0, P_RANGE = P_UPPER-P_LOWER;
+    public static final double I_UPPER = 100.0, I_LOWER = 0.0, I_RANGE = I_UPPER-I_LOWER;
     public static final double I_BOUND_UPPER = 100.0, I_BOUND_LOWER = -100.0, I_BOUND_RANGE = I_BOUND_UPPER-I_BOUND_LOWER;
 
     //JANKIEST SOLUTION EVER
@@ -30,7 +30,17 @@ public class PIDTuner {
         this.pid = pendulum.getPID();
         this.pendulum = pendulum;
         this.originalPos = pendulum.getHead();
-        this.frame = new MainFrame(pendulum, true);
+        this.frame = new MainFrame(pendulum, false);
+    }
+
+    public PIDTuner(InvertedPendulum pendulum, boolean gui){
+        this.pid = pendulum.getPID();
+        this.pendulum = pendulum;
+        this.originalPos = pendulum.getHead();
+        if(gui)
+            this.frame = new MainFrame(pendulum, false);
+        else
+            this.frame = null;
     }
 
     public void trainPID(){
@@ -51,7 +61,9 @@ public class PIDTuner {
             Simulator sim = new Simulator((int) (Main.DT*1000)){
                 @Override
                 public void simulate(){
-                    frame.repaint();
+                    pendulum.step();
+                    if(frame != null)
+                        frame.repaint();
                     totalErrorAbs[0]+=Math.abs(pendulum.getAngleBtwnUnstable());
                 }
             };
